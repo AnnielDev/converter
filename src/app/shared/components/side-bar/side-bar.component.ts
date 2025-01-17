d520fd38c594df0488e8a17b9a4a9f317cc92b6c
Anniel-Reyes <areyes@brisiao.do>
2025-01-16 23:45:37 -0400

    style: sidebar with mat-drawer

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,31 +1,24 @@
 import { Component } from '@angular/core';
-import {
-  Router,
-  RouterOutlet,
-  RouterLink,
-  ActivatedRoute,
-} from '@angular/router';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatIconModule],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+  ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
 })
 export class SideBarComponent {
   currentUrl: string = '';
-
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    this.currentUrl = this.router.url;
-
-    this.router.events.subscribe(() => {
-      this.currentUrl = this.router.url;
-    });
-  }
+  showNav: boolean = false;
   routes = [
     { name: 'Currency', route: '/metric-currency', icon: 'currency_exchange' },
     {
@@ -41,4 +34,13 @@ export class SideBarComponent {
       icon: 'directions_run',
     },
   ];
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.currentUrl = this.router.url;
+
+    this.router.events.subscribe(() => {
+      this.currentUrl = this.router.url;
+    });
+  }
 }
